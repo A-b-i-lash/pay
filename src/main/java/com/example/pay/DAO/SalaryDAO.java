46d2cd419aba47a484c87b5cf71abf58162cbe72
Abilash <abilash2408@gmail.com>
2024-07-11 13:26:48 +0530

    edit

--- src/main/java/com/example/pay/DAO/SalaryDAO.java
+++ src/main/java/com/example/pay/DAO/SalaryDAO.java
@@ -1,8 +1,5 @@
 package com.example.pay.DAO;
 
-import java.math.BigDecimal;
-import java.math.RoundingMode;
-import java.text.DecimalFormat;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,12 +14,11 @@ public class SalaryDAO {
 
     @Autowired
     private SalaryRepo repo;
-    private static final DecimalFormat decfor = new DecimalFormat("0.00");  
 
     public SalaryDTO AddDetails(@RequestBody SalaryDTO Add){
         double presentDays = Add.getWorkingDays() - Add.getAbsentDays();
         Add.setPresentDays(presentDays);
-        double perDaySalary = 15000.00 / 26.00;
+        double perDaySalary = Add.getBasicSalary() / Add.getWorkingDays();
         System.out.println(perDaySalary);
         double totalEarnings = perDaySalary * presentDays;
         Add.setTotalEarnings(Math.round(totalEarnings));
@@ -58,6 +54,38 @@ public class SalaryDAO {
 
     public SalaryDTO UpdateDetails(@RequestBody Integer id,SalaryDTO Update){
         Update.setId(id);
+        double presentDays = Update.getWorkingDays() - Update.getAbsentDays();
+        Update.setPresentDays(presentDays);
+        double perDaySalary = Update.getBasicSalary() / 26;
+        System.out.println(perDaySalary);
+        double totalEarnings = perDaySalary * presentDays;
+        Update.setTotalEarnings(Math.round(totalEarnings));
+        double basicSalary = (totalEarnings / 100) * 60;
+        Update.setBasicSalary(Math.round(basicSalary));
+        double hraAllowance = (basicSalary / 100) * 25;
+        Update.setHraAllowance(Math.round(hraAllowance));
+        double medicalReimbursement = (basicSalary / 100) * 6.67;
+        Update.setMedicalReimbursement(Math.round(medicalReimbursement));
+        double communicationAllowance = (basicSalary / 100) * 5;
+        Update.setCommunicationAllowance(Math.round(communicationAllowance));
+        double ltaAllowance = (basicSalary / 100) * 10;
+        Update.setLtaAllowance(Math.round(ltaAllowance));
+        double specialAllowance = (basicSalary / 100) * 10;
+        Update.setSpecialAllowance(Math.round(specialAllowance));
+        double incentiveAllowance = (basicSalary / 100) * 10;
+        Update.setIncentiveAllowance(Math.round(incentiveAllowance));
+
+        double providentFund = (basicSalary / 100) * 12;
+        Update.setProvidentFund(Math.round(providentFund));
+        double esic = (basicSalary / 100) * 8;
+        Update.setEsic(Math.round(esic));
+        Update.setWelfareFund(100);
+
+        double totalDeductions = providentFund + esic + 100;
+        Update.setTotalDeductions(Math.round(totalDeductions));
+        double netSalary = totalEarnings - totalDeductions;
+        Update.setNetSalary(Math.round(netSalary));
+
         return repo.save(Update);
     }
 
